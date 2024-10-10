7bec66c6d69200dc29e7c1f2abe192c1fc8033ea
carotte-de-l-espace-joueur-star-en-0-0 <quentin.labat@stjoavignon.com>
2024-10-10 11:41:51 +0200

    Update test github.py

--- test github.py
+++ test github.py
@@ -1,2 +1,15 @@
 # Créé par elevessi, le 10/10/2024 en Python 3.7
-
+def codage():
+    txt=input("entrez une phrase")
+    txt=[txt]
+    global code
+    global num
+    for letter in txt:
+        num=ord(letter)
+    [txt]=txt
+    code=str.upper(txt)
+    return code
+    return num
+codage()
+print(code)
+print(num)
