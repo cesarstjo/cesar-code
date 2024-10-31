2333501aea2ac482ec69440eb42d7f57321c6170
cesarstjo <cesar.guerrier@stjoavignon.com>
2024-10-31 12:56:19 +0100

    Update test github.py
    ton code de codage est full buggé dcp j'ai essayer dans refaire un

--- test github.py
+++ test github.py
@@ -13,3 +13,53 @@ def codage():
 codage()
 print(code)
 print(num)
+
+
+
+
+#####DECODAGE######
+def cesar_decode(message, decalage):
+    resultat = ""
+    for lettre in message:
+        if lettre.isalpha():
+            lettre = lettre.upper()
+            offset = 65
+            lettre_decodee = chr((ord(lettre) - offset - decalage) % 26 + offset)
+            resultat += lettre_decodee
+        else:
+
+            resultat += lettre
+    return resultat
+
+
+print("=== Décodeur du chiffrement de César ===")
+message_code = input("Entrez le message codé : ")
+decalage = int(input("Entrez le décalage utilisé pour le codage : "))
+message_decode = cesar_decode(message_code, decalage)
+print("Le message décodé est :", message_decode)
+
+
+
+#####CODAGE######
+def cesar_encode(message, decalage):
+    resultat = ""
+    for lettre in message:
+        if lettre.isalpha():
+            if 'a' <= lettre <= 'z':
+                lettre = chr(ord(lettre) - 32)
+
+            offset = 65
+            lettre_encodee = chr((ord(lettre) - offset + decalage) % 26 + offset)
+            resultat += lettre_encodee
+        else:
+
+            resultat += lettre
+    return resultat
+
+print("=== Encodeur du chiffrement de César ===")
+message_clair = input("Entrez le message à encoder : ")
+decalage = int(input("Entrez le décalage pour l'encodage : "))
+message_code = cesar_encode(message_clair, decalage)
+print("Le message encodé est :", message_code)
+
+
